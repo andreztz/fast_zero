85a3a5d371e7cbcd02c43b5594e3c577b4e52eb1
André P. Santos <andreztz@gmail.com>
2024-06-21 19:03:52 -0300

    Adição da rota para resposta HTML e test correspondente
    - Implementa uma nova rota `/html/` que retorna uma resposta formatada
    em HTML.
    - O teste garante:
        - o código da resposta 200 (OK)
        - o tipo do conteúdo da reposta correto (text/html; charset=utf-8)
        - a resposta HTML, contendo 'Olá Mundo!'

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,24 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     response = client.get("/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Olá Mundo!"}
+
+
+def test_html_deve_retornar_paggina_html_contendo_ola_mundo():
+    response = client.get("/html")
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+    assert (
+        response.text
+        == """
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Olá Mundo!</title>
+            </head>
+            <body>
+                <h1>Olá Mundo!</h1>
+            </body>
+        </html>
+    """
+    )
+    assert "Olá Mundo!" in response.text
