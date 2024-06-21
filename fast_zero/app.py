f92fafe463184999fccd87f20fdb0768dae40c38
André P. Santos <andreztz@gmail.com>
2024-06-21 17:42:17 -0300

    Adição de schema para resposta da rota `/`

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,9 +1,12 @@
 from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/", status_code=HTTPStatus.OK)
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá mundo!"}
