43d4567655c1ee9e91c538b4382860668dc74f55
André P. Santos <andreztz@gmail.com>
2024-06-24 01:51:37 -0300

    Implementado teste para a rota `/users/`
    - O teste garante:
        - o código de resposta 201 (CREATED).
        - usuário criado e dados corretos na resposta.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,33 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
-client = TestClient(app)
+
+@pytest.fixture()
+def client():
+    return TestClient(app)
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get("/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Olá Mundo!"}
+
+
+def test_create_user(client):
+    data = {
+        "username": "test",
+        "password": "super_secret",
+        "email": "test@mail.com",
+    }
+
+    response = client.post("/users/", json=data)
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "id": 1,
+        "username": "test",
+        "email": "test@mail.com",
+    }
